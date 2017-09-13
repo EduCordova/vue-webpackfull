53f1bc283bb8aacadeb4178d2f797ceabb77a6f7
EduCordova <kde1690@gmail.com>
2017-09-12 22:12:46 -0500

    new cmmt

--- src/components/notfound.vue
+++ src/components/notfound.vue
@@ -1,4 +1,7 @@
 <template>
-  
+<div>
+
+  <h4>Pagina no Encontrada</h4>
   <img src="https://media.giphy.com/media/l378e8KK6ogEkCx5S/giphy.gif">
+</div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ import notfound from '@/components/notfound'
 Vue.use(Router)
 
 export default new Router({
-  mode:"history",
   routes: [
     {path: '/',component: home},
     {path: '/adopcion',component: adopcion},
